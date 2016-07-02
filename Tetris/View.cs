c1c2cd049cc909138e504d5884261cee9b954a67
Burakovivan <Burakovivan@gmail.com>
2016-07-02 21:30:30 +0300

    delete some shit

--- Tetris/View.cs
+++ Tetris/View.cs
@@ -16,10 +16,7 @@ namespace Tetris
             /// Bricks that was fallen down
             /// </summary>
             public static bool[,] Commited { get; private set; }
-            /// <summary>
-            /// Brick that flying down
-            /// </summary>
-            public static bool[,] InTransit { get; private set; }
+            
 
             static Brick falling;
             static public int Speed { get; private set; }
