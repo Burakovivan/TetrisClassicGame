d3bbc48814b334173201fb56f5c9bd7b7d9c3a4b
Burakovivan <Burakovivan@gmail.com>
2016-07-02 21:32:32 +0300

    prosto

---
+++ Tetris/Field.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tetris
+{
+    static class Field
+    {
+        /// <summary>
+        /// Bricks that was fallen down
+        /// </summary>
+        public static bool[,] Commited { get; private set; }
+
+
+        static Brick falling;
+        static public int Speed { get; private set; }
+        static public int Scoore { get; private set; }
+
+        //проверка падания
+        //отлов управления (движение, поворот)
+        //проверка линий
+        //подсчет очков
+
+    }
+}
--- Tetris/View.cs
+++ Tetris/View.cs
@@ -10,24 +10,7 @@ namespace Tetris
     {
         public static OpenGL Canvas { get; set; }
 
-        static class Field 
-        {
-            /// <summary>
-            /// Bricks that was fallen down
-            /// </summary>
-            public static bool[,] Commited { get; private set; }
-            
-
-            static Brick falling;
-            static public int Speed { get; private set; }
-            static public int Scoore { get; private set; }
-            
-            //проверка падания
-            //отлов управления (движение, поворот)
-            //проверка линий
-            //подсчет очков
-            
-        }
+        
 
     }
     
